3406fdbf1d5b0583c47e6f4df2803b2756442f29
Ornellah <ornella.helvas@outlook.fr>
2025-02-08 22:18:36 +0100

    json fix

--- clients/client_python_iris.py
+++ clients/client_python_iris.py
@@ -4,7 +4,7 @@ data = {
     "SepalLengthCm": 1.8,
     "SepalWidthCm": 2.6,
     "PetalLengthCm": 2.1,
-    "PetalWidthCm": 3.5,
+    "PetalWidthCm": 3.5
 }
 reponse = requests.post("http://127.0.0.1:5000/iris_species", json=data)
 print(reponse.json())
